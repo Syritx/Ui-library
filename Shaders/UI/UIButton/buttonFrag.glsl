c8ef47923559c9e8bde7bd580e4307d06c369e38
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-12-07 09:15:23 -0500

    updated shaders

--- Shaders/UI/UIButton/buttonFrag.glsl
+++ Shaders/UI/UIButton/buttonFrag.glsl
@@ -2,5 +2,5 @@
 
 out vec4 frag_Color;
 void main() {
-    frag_Color = vec4(0.0,0.0,1.0,1.0);
+    frag_Color = vec4(0.0,1.0,0.55,1.0);
 }
--- Shaders/UI/fragmentUIShader.glsl
+++ Shaders/UI/fragmentUIShader.glsl
@@ -2,5 +2,5 @@
 
 out vec4 frag_Color;
 void main() {
-    frag_Color = vec4(0.45,0.0,0.75,1.0);
+    frag_Color = vec4(0.1,0.1,0.1,1.0);
 }
